bdf135471f3a44a7347c62f852fcd12a60effbd3
swastik007 <swastik.nakarmi@gmail.com>
2024-12-05 13:42:00 +0545

    latest updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,44 @@
-/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeInLeft: {
+          '0%': { opacity: '0', transform: 'translateX(-100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        fadeInRight: {
+          '0%': { opacity: '0', transform: 'translateX(100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        slideInLeft: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slideInRight: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slideInTop: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        slideInBottom: {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeInLeft: 'fadeInLeft 1s ease-out',
+        fadeInRight: 'fadeInRight 1s ease-out',
+        slideInLeft: 'slideInLeft 1s ease-in',
+        slideInRight: 'slideInRight 1s ease-in',
+        slideInTop: 'slideInTop 1s ease-in',
+        slideInBottom: 'slideInBottom 1s ease-in',
+      },
+    },
   },
   plugins: [],
-}
-
+};
